74a0e15ead8d3ad1421c263dd73ba339cb795700
Jacob H Routzong <jhroutzong@outlook.com>
2022-04-27 14:40:24 -0500

    completed drag button effect

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct OnboardingView: View {
     
+    // MARK: - PROPERTY
+    
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
+    
+    // MARK: - BODY
     
     var body: some View {
         ZStack {
@@ -39,14 +46,7 @@ struct OnboardingView: View {
                 // MARK: - CENTER
                 
                 ZStack {
-                    ZStack {
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 40)
-                            .frame(width: 260, height: 260, alignment: .center)
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 80)
-                            .frame(width: 260, height: 260, alignment: .center)
-                    } //: ZSTACK
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
                     
                     Image("character-1")
                         .resizable()
@@ -82,7 +82,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         
                         Spacer()
                     }
@@ -101,19 +101,37 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded { _ in
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                    
+                                    buttonOffset = 0
+                                }
+                        ) //: GESTURE
                         Spacer()
                     } //: HSTACK
                     
                 } //: Footer
-                .frame(height:80, alignment: .center)
+                .frame(width: buttonWidth,  height:80, alignment: .center)
                 .padding()
             } //: VSTACK
         } //: ZSTACK
     }
 }
+
+// MARK: - PREVIEW
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
